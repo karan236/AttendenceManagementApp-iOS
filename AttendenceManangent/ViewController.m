cfb88c07485d967737a797690258f49a3c30f228
karan236 <karanghorai1997@gmail.com>
2021-06-23 18:35:14 +0530

    Completed home page UI

--- AttendenceManangent/ViewController.m
+++ AttendenceManangent/ViewController.m
@@ -8,6 +8,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *takeAttendenceButton;
+@property (strong, nonatomic) IBOutlet UIButton *viewAttendenceRecordButton;
+@property (strong, nonatomic) IBOutlet UIButton *topicsTaughtButton;
+@property (strong, nonatomic) IBOutlet UIButton *topicsMissedByStudentButton;
+@property (strong, nonatomic) IBOutlet UIButton *studentsListButton;
+@property (strong, nonatomic) IBOutlet UIButton *attendenceRecordOfAStudent;
+@property (strong, nonatomic) IBOutlet UIButton *deleteStudentButton;
+@property (strong, nonatomic) IBOutlet UIButton *updateAttendenceButton;
+@property (strong, nonatomic) IBOutlet UIButton *addStudentButton;
 
 @end
 
@@ -15,7 +24,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+//    Home Page buttons Configuration
+    _takeAttendenceButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _takeAttendenceButton.titleLabel.numberOfLines = 2;
+//    [_takeAttendenceButton setTitle:@"Take\nAttendence" forState:UIControlStateNormal];
+    _takeAttendenceButton.titleLabel.adjustsFontForContentSizeCategory = YES;
+    _takeAttendenceButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _takeAttendenceButton.layer.cornerRadius = _takeAttendenceButton.bounds.size.height/8;
+    
+    _viewAttendenceRecordButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _viewAttendenceRecordButton.titleLabel.numberOfLines = 3;
+//    [_viewAttendenceRecordButton setTitle:@"View\nAttendence\nRecord" forState:UIControlStateNormal];
+    _viewAttendenceRecordButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _viewAttendenceRecordButton.layer.cornerRadius = _viewAttendenceRecordButton.bounds.size.height/8;
+    
+    _topicsTaughtButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _topicsTaughtButton.titleLabel.numberOfLines = 2;
+//    [_topicsTaughtButton setTitle:@"Topics\nTaught" forState:UIControlStateNormal];
+    _topicsTaughtButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _topicsTaughtButton.layer.cornerRadius = _topicsTaughtButton.bounds.size.height/8;
+    
+    _topicsMissedByStudentButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _topicsMissedByStudentButton.titleLabel.numberOfLines = 3;
+//    [_topicsMissedByStudentButton setTitle:@"Topics\nMissed by\nStudent" forState:UIControlStateNormal];
+    _topicsMissedByStudentButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _topicsMissedByStudentButton.layer.cornerRadius = _topicsMissedByStudentButton.bounds.size.height/8;
+    
+    _studentsListButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _studentsListButton.titleLabel.numberOfLines = 2;
+//    [_studentsListButton setTitle:@"Students\nList" forState:UIControlStateNormal];
+    _studentsListButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _studentsListButton.layer.cornerRadius = _studentsListButton.bounds.size.height/8;
+    
+    _attendenceRecordOfAStudent.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _attendenceRecordOfAStudent.titleLabel.numberOfLines = 3;
+//    [_attendenceRecordOfAStudent setTitle:@"Attendence\nRecord of a\nStudent" forState:UIControlStateNormal];
+    _attendenceRecordOfAStudent.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _attendenceRecordOfAStudent.layer.cornerRadius = _attendenceRecordOfAStudent.bounds.size.height/8;
+    
+    _deleteStudentButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _deleteStudentButton.titleLabel.numberOfLines = 2;
+//    [_deleteStudentButton setTitle:@"Delete\nStudent" forState:UIControlStateNormal];
+    _deleteStudentButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _deleteStudentButton.layer.cornerRadius = _deleteStudentButton.bounds.size.height/8;
+    
+    _updateAttendenceButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _updateAttendenceButton.titleLabel.numberOfLines = 2;
+//    [_updateAttendenceButton setTitle:@"Update\nStudent" forState:UIControlStateNormal];
+    _updateAttendenceButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    _updateAttendenceButton.layer.cornerRadius = _updateAttendenceButton.bounds.size.height/8;
+    
+    _addStudentButton.layer.cornerRadius = _addStudentButton.bounds.size.height/8;
+    
 }
 
 
